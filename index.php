4849cd7f8480fa81564c56342394cc9750689ecc
wwnp <iamtodaysad@gmail.com>
2022-04-21 18:32:25 +0300

    test

--- index.php
+++ index.php
@@ -1,15 +1,34 @@
 <?php
-include_once('function.php');
-$articles = getArts();
+$isSend = false;
+$err = '';
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    echo '123312313';
+    $name = trim($_POST["name"]);
+    $phone = trim($_POST["phone"]);
+    $dt = date('Y-d-m H:i:s');
+    if ($name === '' || $phone === '') {
+        $err = 'fill';
+    } else {
+        $mainnody = "$dt\n$phone\n$name";
+        mail('1@1.ru', 'New ap', $mainnody);
+        $isSend = true;
+    }
+} else {
+    echo "METHOD GET";
+} // METHOD GET
+echo $isSend;
+echo '<pre>';
+print_r($_POST);
+echo '</pre>';
 
 ?>
-<div>
-  <? foreach ($articles as $key => $value) : ?>
-
-  <div class="article">
-    <h2><?= $value['id'] ?><?= $value['name'] ?></h2>
-    <a href="article.php?id=<?= $value['id'] ?>">Read more</a>
-  </div>
-
-  <? endforeach ?>
-</div>
+<?if ($isSend): ?>
+<h1>DONE</h1>
+<?else: ?>
+<form method="POST">
+  Name: <br><input type="text" name="name">
+  Phone:<br><input type="text" name="phone">
+  <button type="submit">Send</button>
+  <p><?=$err?></p>
+</form>;
+<?endif;?>
