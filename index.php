0633fef7d419d5f421a974b0d461b9d658c27746
wwnp <iamtodaysad@gmail.com>
2022-04-21 17:13:56 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+include_once('function.php');
+$articles = getArts();
+
+?>
+<div>
+  <? foreach ($articles as $key => $value) : ?>
+  <div class="article">
+    <h2><?= $value['id'] ?><?= $value['name'] ?></h2>
+    <a href="article.php?id=<?= $value['id'] ?>">Read more</a>
+  </div>
+  <? endforeach ?>
+</div>
