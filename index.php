1204f9cd83722f491d3de37584f2ee2823acb1e9
wwnp <iamtodaysad@gmail.com>
2022-04-21 17:18:18 +0300

    first commit

--- index.php
+++ index.php
@@ -5,9 +5,11 @@ $articles = getArts();
 ?>
 <div>
   <? foreach ($articles as $key => $value) : ?>
+
   <div class="article">
     <h2><?= $value['id'] ?><?= $value['name'] ?></h2>
     <a href="article.php?id=<?= $value['id'] ?>">Read more</a>
   </div>
+
   <? endforeach ?>
 </div>
