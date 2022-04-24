32da97fd7aa9a40c032b78f49abe321e8ca27a10
wwnp <iamtodaysad@gmail.com>
2022-04-24 23:59:57 +0300

    rf

--- index.php
+++ index.php
@@ -1,42 +1,14 @@
 <?php
-
-include_once('model/apps.php');
-
-$isSend = false;
-$err = '';
-
-if($_SERVER['REQUEST_METHOD'] === 'POST'){
-	$name = trim($_POST['name']);
-	$phone = trim($_POST['phone']);
-	
-	if($name === '' || $phone === ''){
-		$err = 'Заполните все поля!';
-	}
-	else if(mb_strlen($name, 'UTF8') < 2){
-		$err = 'Имя не короче 2 символов!';
-	}
-	else{
-		addApp($name, $phone);
-		$isSend = true;
-	}
+$f = fopen('db.txt', 'r');
+$total = 0;
+$cnt = 0;
+while (!feof($f)) {
+  $str = rtrim(fgets($f));
+  if($str !== ''){
+    $total += (string) $str;
+    $cnt++;
+  }
 }
-else{
-	$name = '';
-	$phone = '';
-}
-
-?>
-<div class="form">
-	<? if($isSend): ?>
-		<p>Your app is done!</p>
-	<? else: ?>
-		<form method="post">
-			Name:<br>
-			<input type="text" name="name" value="<?=$name?>"><br>
-			Phone:<br>
-			<input type="text" name="phone" value="<?=$phone?>"><br>
-			<button>Send</button>
-			<p><?=$err?></p>
-		</form>
-	<? endif; ?>
-</div>
+echo memory_get_usage() . '<br>';
+echo $total / $cnt;
+fclose($f);
