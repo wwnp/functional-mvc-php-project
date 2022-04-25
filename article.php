b9b6be5285ebfaed68b55cb34cfcc83e72025273
wwnp <iamtodaysad@gmail.com>
2022-04-25 07:14:14 +0300

    rf

---
+++ article.php
@@ -0,0 +1,24 @@
+<?php
+
+include_once 'functions.php';
+$articles = getArticles();
+
+$id = (int) $_GET["id"] ?? '';
+$post = $articles[$id] ?? null;
+$hasPost = $post ?? null;
+?>
+<div class="content">
+  <?if ($hasPost): ?>
+    <h1><?=$post['title']?></h1>
+    <div><?=$post['content']?></div>
+    <hr>
+    <a href="edit.php?id=<?=$id?>">Edit</a>
+    <a href="delete.php?id=<?=$id?>">Remove</a>
+  <?else: ?>
+    <div class="e404">
+      <h1>Страница не найдена!</h1>
+    </div>
+  <?endif?>
+</div>
+<hr>
+<a href="index.php">Main page</a>
