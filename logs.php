2c11d550fbb8373826a3083787bf93b66f7e42f6
wwnp <iamtodaysad@gmail.com>
2022-04-26 15:32:29 +0300

    rf

--- logs.php
+++ logs.php
@@ -1,47 +1,9 @@
 <?php 
   include_once 'model/logging.php';
   addLogVisit();
-  $allFiles = scandir('db/visits'); // Or any other directory
+  $allFiles = scandir('db/visits'); 
   $files = array_diff($allFiles, [ 0 => '.', 1 =>  '..']);
-  $outArray = fromTxtToArray($files);
 ?>
-<table>
-  <tr>
-    <td>Date</td>
-    <td>Time</td>
-    <td>IP</td>
-    <td>URI</td>
-  </tr>
-  <? foreach($outArray as $key => $value ): ?>
-    <tr>
-      <td>
-        <?= 
-        isset($value['date']) 
-          ? $value['date']
-          : "X"
-        ?>
-      </td>
-      <td>
-        <?= 
-        isset($value['time']) 
-          ? $value['time']
-          : "X"
-        ?>
-      </td>
-      <td>
-        <?= 
-        isset($value['ip']) 
-          ? $value['ip']
-          : "X"
-        ?>
-      </td>
-      <td>
-        <?= 
-        isset($value['uri']) 
-          ? $value['uri']
-          : "X"
-        ?>
-      </td>
-    </tr>
-  <? endforeach; ?>
-</table>
+<? foreach($files as $key => $value ): ?>
+  <a href="log.php?date=<?= $value ?>"><?= $value ?></a><br>
+<? endforeach; ?>   
--- model/logging.php
+++ model/logging.php
@@ -1,5 +1,39 @@
 <?php 
+  function addLogVisit(): bool {
+    $date = date('Y-m-d');
+    $time = date('h:i:s');
+    $log = [
+      'date' => $date,
+      'time' => $time,
+      'ip' => $_SERVER['SERVER_ADDR'],
+      'uri' => $_SERVER['REQUEST_URI'],
+    ];
+    $log = json_encode($log) . "\n";
+    file_put_contents("db/visits/{$date}.txt", $log, FILE_APPEND );
+    return true;
+  }
 
+  function txtToArraySingle($file): array {
+    $str = fread($file, filesize("db/visits/{$_GET['date']}"));
+    $lines = explode("\n", $str);
+    array_pop($lines);
+    $apps = [];
+    foreach($lines as $key => $value){
+      $parts = json_decode($value,true);
+      $apps[] = [
+        'date' => $parts['date']  ?? null,
+        'time' => $parts['time'] ?? null,
+        'ip' => $parts['ip'] ?? null,
+        'uri' => $parts['uri'] ?? null,
+      ];
+    }
+    return $apps;
+  }
+
+
+
+
+  // FOR ALL LOGS IN ONE PAGE
   function fromTxtToArray(array $files): array {
     $apps = [];
     foreach ($files as $key => $value) {
@@ -14,7 +48,6 @@
     }
     return array_reverse($apps);
   }
-
   function fromLineToArray(string $line): array {
     $parts = json_decode($line, true);
     return [
@@ -23,19 +56,5 @@
       'ip' => $parts['ip'] ?? null,
       'uri' => $parts['uri'] ?? null,
     ];
-  }
-
-  function addLogVisit(): bool {
-    $date = date('Y-m-d');
-    $time = date('h:i:s');
-    $log = [
-      'date' => $date,
-      'time' => $time,
-      'ip' => $_SERVER['SERVER_ADDR'],
-      'uri' => $_SERVER['REQUEST_URI'],
-    ];
-    $log = json_encode($log) . "\n";
-    file_put_contents("db/visits/{$date}.txt", $log, FILE_APPEND );
-    return true;
   }
 ?>
