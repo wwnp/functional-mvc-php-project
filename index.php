c2b6368bf27819ed92c572fb3be39a4ef57b49f3
wwnp <iamtodaysad@gmail.com>
2022-05-03 03:48:35 +0300

    rf

--- index.php
+++ index.php
@@ -1,6 +1,10 @@
 <?php 
   include_once('model/db.php');
   $db = dbInstance();
+  $db2 = dbInstance();
+
+  $res1  = $db->query('Select CONNECTION_ID()')->fetch();
+  $res2 = $db2->query('Select CONNECTION_ID()')->fetch();
 
   $sql = "
     SELECT * FROM messages ORDER BY dt_add DESC;
--- model/db.php
+++ model/db.php
@@ -1,9 +1,12 @@
 <?php 
   function dbInstance(): PDO {
-    $db = new PDO('mysql:host=localhost;dbname=chat', "root", "", [
-      PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
-    ]);
-    $db->exec('SET NAMES UTF8');
+    static $db;
+    if($db === null){
+      $db = new PDO('mysql:host=localhost;dbname=chat', "root", "", [
+        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+      ]);
+      $db->exec('SET NAMES UTF8');
+    }
     return $db;
   }
   function dbQuery(string $sql, array $params = []): PDOStatement {
