92bbbd1530b82372aac770b6c091b472e81521ca
wwnp <iamtodaysad@gmail.com>
2022-05-05 19:23:01 +0300

    rf

--- auxillary.php
+++
@@ -1,9 +0,0 @@
-<?php 
-  function fillFieldsFromPost() {
-    $out = [];
-    foreach ($_POST as $key => $value) {
-      $out[$key] = trim($value);
-    }
-    return $out;
-  }
-?>
--- delete.php
+++
@@ -1,5 +0,0 @@
-<?php 
-  include_once('model/articles.php');
-  deleteArticle($_GET['id']);
-  header("location: index.php");
-?>
